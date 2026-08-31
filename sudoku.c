21d33876dde2407caba4cb16b8fed455e1dcda23
Luka Krmpotić <luka.krmpotic@gmail.com>
2026-08-31 21:18:02 +0200

    reduce scope

--- sudoku.c
+++ sudoku.c
@@ -39,7 +39,6 @@ void solve_table(int table[9][9]) {
   int solved_flag;
   do {
     num_changes = 0;
-    int i, j;
     solved_flag = 1;
 
     for (int n = 0; n < 9; n++) {
@@ -48,22 +47,23 @@ void solve_table(int table[9][9]) {
           int value = (-1) * table[n][m];
           int bit = 1 << (value - 1);
 
-          for (i = 0; i < 9; i++) {  // COLUMN
+          for (int i = 0; i < 9; i++) {  // COLUMN
             if (table[i][m] > 0 && ((table[i][m] & bit) == bit)) {
               table[i][m] = table[i][m] - bit;
               num_changes += 1;
             }
           }
 
-          for (j = 0; j < 9; j++) {  // ROW
+          for (int j = 0; j < 9; j++) {  // ROW
             if (table[n][j] > 0 && ((table[n][j] & bit) == bit)) {
               table[n][j] = table[n][j] - bit;
               num_changes += 1;
             }
           }
 
-          for (i = 3 * (int)(n / 3); i < 3 * (int)(n / 3) + 3; i++) {  // SQUARE
-            for (j = 3 * (int)(m / 3); j < 3 * (int)(m / 3) + 3; j++) {
+          for (int i = 3 * (int)(n / 3); i < 3 * (int)(n / 3) + 3;
+               i++) {  // SQUARE
+            for (int j = 3 * (int)(m / 3); j < 3 * (int)(m / 3) + 3; j++) {
               if (table[i][j] > 0 && ((table[i][j] & bit) == bit)) {
                 table[i][j] = table[i][j] - bit;
                 num_changes += 1;
